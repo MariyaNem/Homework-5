fed458f2567a280d65ca9574a2fb9be6d6347f5f
Mariya Nemzorova <nemzorova777@mail.ru>
2023-01-22 20:19:33 +0700

    Solve task 36

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -2,3 +2,40 @@
 // Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
+
+int[] array = CreateArrayRndInt(4);
+PrintArray(array);
+int sumNumbersOddPositions = SumNumbersOddPositions(array);
+Console.Write($"Сумма элементов массива стоящих на нечётных позициях = {sumNumbersOddPositions}");
+
+int[] CreateArrayRndInt(int size)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(-99, 99 + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
+
+int SumNumbersOddPositions(int[] arr)
+{
+    int sum = 0;
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (i % 2 != 0) sum += arr[i];
+    }
+    return sum;
+}
